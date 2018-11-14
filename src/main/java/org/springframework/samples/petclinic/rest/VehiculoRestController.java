4fb70e04adbe92e6aea322c53312196cb7fe8f38
MParrar <44446789+MParrar@users.noreply.github.com>
2018-11-14 16:47:59 -0300

    VehiculoRestController metodo mostrar todo

--- src/main/java/org/springframework/samples/petclinic/rest/VehiculoRestController.java
+++ src/main/java/org/springframework/samples/petclinic/rest/VehiculoRestController.java
@@ -1,8 +1,19 @@
 package org.springframework.samples.petclinic.rest;
 
+import java.util.Collection;
+
 import javax.validation.Valid;
+
+import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.samples.petclinic.service.VehiculoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.samples.petclinic.model.Vehiculo;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/vehiculos")
 
 public class VehiculoRestController {
-
+	
+	@Autowired
+	private VehiculoService VehiculoService;
+	
+	
+	@RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<Collection<Vehiculo>> getVehiculo(){
+		Collection<Vehiculo> vehiculos = this.VehiculoService.findAllVehiculos();
+		if(vehiculos.isEmpty()){
+			return new ResponseEntity<Collection<Vehiculo>>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Collection<Vehiculo>>(vehiculos, HttpStatus.OK);
+	}
 }
