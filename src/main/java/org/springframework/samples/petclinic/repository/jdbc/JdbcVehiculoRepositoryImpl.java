e39b47b4b5ced571f11fadadef24613e6418f54c
diegoalbertopp <diego.poblete1501@alumnos.ubiobio.cl>
2018-11-14 16:01:13 -0300

    JdbcVehiculoRepositoryImpl

--- src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcVehiculoRepositoryImpl.java
+++ src/main/java/org/springframework/samples/petclinic/repository/jdbc/JdbcVehiculoRepositoryImpl.java
@@ -9,28 +9,36 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.samples.petclinic.model.Vehiculo;
+import org.springframework.samples.petclinic.repository.VehiculoRepository;
 
-public class JdbcVehiculoRepositoryImpl {
+public class JdbcVehiculoRepositoryImpl implements VehiculoRepository{
 	
 	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     
-    @Autowired
-    public Collection<Vehiculo> findAll() throws DataAccessException {
+	@Override
+	public Vehiculo findVehiculoByPatente(int patente) throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Vehiculo> findAllVehiculos() throws DataAccessException {
 		List<Vehiculo> vehiculos = this.namedParameterJdbcTemplate.query(
 	            "SELECT * FROM vehiculo",
 	            new HashMap<String, Object>(),
 	            BeanPropertyRowMapper.newInstance(Vehiculo.class));
-		/*for(Vehiculo vehiculo : vehiculos) {
-            loadVehiculo(vehiculos);
-        }*/
 	    return vehiculos;
 	}
-    
-    /*private void loadVehiculo(List<Vehiculo> vehiculos) {
-        for (Vehiculo  vehiculo :  vehiculos) {
-            loadVehiculo( vehiculos);
-        }
-    }*/
 
+	@Override
+	public void saveVehiculo(Vehiculo vehiculo) throws DataAccessException {
+		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public void deleteVehiculo(Vehiculo vehiculo) throws DataAccessException {
+		// TODO Auto-generated method stub
+		
+	}
 }
