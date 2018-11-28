04f10d69741c3cf0947a94ec96dc3d9127d454b6
MParrar <44446789+MParrar@users.noreply.github.com>
2018-11-28 16:15:30 -0300

    Controlador ServicioFlete

--- src/main/java/org/springframework/samples/petclinic/rest/servicioFleteRestController.java
+++ src/main/java/org/springframework/samples/petclinic/rest/servicioFleteRestController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.samples.petclinic.model.Chofer;
 import org.springframework.samples.petclinic.model.servicioFlete;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.samples.petclinic.service.ChoferService;
 import org.springframework.samples.petclinic.service.servicioFleteService;
 import java.util.Collection;
 
@@ -20,4 +22,29 @@ import java.util.Collection;
 
 public class servicioFleteRestController {
 
+	@Autowired
+	private servicioFleteService servicioFService;
+	
+	@PreAuthorize( "hasRole(@roles.CHOFER_ADMIN)" )
+	@RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<Collection<servicioFlete>> getServicio() {
+		Collection<servicioFlete> serviciosF = this.servicioFService.findAllServicioFlete();
+		if (serviciosF.isEmpty()) {
+			return new ResponseEntity<Collection<servicioFlete>>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Collection<servicioFlete>>(serviciosF, HttpStatus.OK);
+	}
+	
+	@PreAuthorize( "hasRole(@roles.CHOFER_ADMIN)" )
+	@RequestMapping(value = "/{servicioId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<servicioFlete> getServicio(@PathVariable("servicioId") int servicoId) {
+		servicioFlete servicioF = null;
+		servicioF = this.servicioFService.findServicioFleteById(servicoId);
+		if (servicioF == null) {
+			return new ResponseEntity<servicioFlete>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<servicioFlete>(servicioF, HttpStatus.OK);
+	}
+	
+	
 }
